df1a62c2a304c5e9cd7acca3cf12ee78a3e2b8d7
Dylan Thomas <dylanthomas1108@gmail.com>
2024-04-09 10:54:14 +0100

    tsting features fro assignment

---
+++ Assignment/Testing/StartWindowCombo.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+from TestUI import *
+
+app = QApplication([])
+w = QMainWindow()
+w.setWindowTitle('Incrementer')
+ex = Ui_MainWindow()
+ex.setupUi(w)
+w.resize(500, 800)
+w.show()
+app.exec_()
